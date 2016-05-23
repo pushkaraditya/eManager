3a0fc27fb1c3c77a1c4a71fef7756d41cd5725a9
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-05-23 12:55:18 +0530

    completed 2.2 Getting started

--- eManager.Web/Controllers/AccountController.cs
+++ eManager.Web/Controllers/AccountController.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Transactions;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
-using WebMatrix.WebData;
 using eManager.Web.Filters;
 using eManager.Web.Models;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
 
 namespace eManager.Web.Controllers
 {
---
+++ eManager.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using eManager.Domain;
+
+namespace eManager.Web.Controllers
+{
+  public class DepartmentController : Controller
+  {
+    private readonly IDepartmentDataSource _db;
+    public DepartmentController(IDepartmentDataSource db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Detail(int id)
+    {
+      var model = _db.Departments.Single(d => d.Id == id);
+      return View(model);
+    }
+  }
+}
