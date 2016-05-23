b07f9942abcc990779bffe21cda797b8731a7aaf
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-05-23 12:33:39 +0530

    completed 1.5 showing data

---
+++ eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace eManager.Domain
+{
+  public interface IDepartmentDataSource
+  {
+    IQueryable<Employee> Employees { get; }
+    IQueryable<Department> Departments { get; }
+  }
+}
--- eManager.Web/Controllers/HomeController.cs
+++ eManager.Web/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using eManager.Domain;
+using eManager.Web.Infrastructure;
 
 namespace eManager.Web.Controllers
 {
   public class HomeController : Controller
   {
+    private IDepartmentDataSource _db;
+    public HomeController(IDepartmentDataSource db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
-      return View();
+      var allDepartments = _db.Departments;
+      return View(allDepartments);
     }
 
     public ActionResult About()
