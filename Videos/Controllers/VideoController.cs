8ca7598ceb456ba729e9b3e3acca40bb00e0b81c
Pushkar Aditya <pushkar.aditya@askiitians.com>
2016-05-24 10:14:38 +0530

    completed 4.3 getting started

---
+++ Videos/Controllers/VideoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Videos.Controllers
+{
+  public class VideoController : ApiController
+  {
+    // GET api/video
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/video/5
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/video
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/video/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/video/5
+    public void Delete(int id)
+    {
+    }
+  }
+}
